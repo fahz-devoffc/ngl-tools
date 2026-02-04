838f7f0cf3a34f2616ff76c117ef26b4abd2864a
Fahz Kang Rinem <fahrifakhrullah@gmail.com>
2026-02-04 20:44:45 +0700

    Update dlsp.js

--- api/dlsp.js
+++ api/dlsp.js
@@ -1,1 +1,7 @@
-
+export default async function(req,res){
+ const {url}=req.query;
+ const api=`https://api.zenitsu.web.id/api/download/spotify?url=${url}`;
+ const r=await fetch(api); const d=await r.text();
+ res.setHeader("Access-Control-Allow-Origin","*");
+ res.status(200).send(d);
+}
