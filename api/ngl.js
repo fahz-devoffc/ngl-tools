e70cb4614a67afb76e8ed009b94bc34c0b0ed8ef
Fahz Kang Rinem <fahrifakhrullah@gmail.com>
2026-02-04 20:48:04 +0700

    Update ngl.js

--- api/ngl.js
+++ api/ngl.js
@@ -1,1 +1,7 @@
-
+export default async function(req,res){
+ const {username,text,amount}=req.query;
+ const api=`https://api.zenitsu.web.id/api/tools/nglspam?username=${username}&text=${text}&amount=${amount}`;
+ const r=await fetch(api); const d=await r.text();
+ res.setHeader("Access-Control-Allow-Origin","*");
+ res.status(200).send(d);
+}
