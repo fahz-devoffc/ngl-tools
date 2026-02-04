50be3b67a2513eed6fc17fd39a17a5fc46e4e310
Fahz Kang Rinem <fahrifakhrullah@gmail.com>
2026-02-04 20:46:18 +0700

    Update hd.js

--- api/hd.js
+++ api/hd.js
@@ -1,1 +1,7 @@
-
+export default async function(req,res){
+ const {img}=req.query;
+ const api=`https://api.zenitsu.web.id/api/tools/hd?imgUrl=${img}`;
+ const r=await fetch(api); const d=await r.text();
+ res.setHeader("Access-Control-Allow-Origin","*");
+ res.status(200).send(d);
+}
