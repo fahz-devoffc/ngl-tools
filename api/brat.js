ffc1952156c4464f0b34c1e50ee26be5f3693b98
Fahz Kang Rinem <fahrifakhrullah@gmail.com>
2026-02-04 20:44:13 +0700

    Update brat.js

--- api/brat.js
+++ api/brat.js
@@ -1,1 +1,7 @@
-
+export default async function(req,res){
+ const {text,color}=req.query;
+ const api=`https://api.zenitsu.web.id/api/maker/brat?text=${text}&words=${color}`;
+ const r=await fetch(api); const d=await r.text();
+ res.setHeader("Access-Control-Allow-Origin","*");
+ res.status(200).send(d);
+}
