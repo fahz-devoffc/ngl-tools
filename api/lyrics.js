de2fc3b544bc95d8ab10fa7eb1a16d7701bc0277
Fahz Kang Rinem <fahrifakhrullah@gmail.com>
2026-02-04 20:47:27 +0700

    Update lyrics.js

--- api/lyrics.js
+++ api/lyrics.js
@@ -1,1 +1,7 @@
-
+export default async function(req,res){
+ const {q}=req.query;
+ const api=`https://api.zenitsu.web.id/api/search/lyrics?q=${q}`;
+ const r=await fetch(api); const d=await r.text();
+ res.setHeader("Access-Control-Allow-Origin","*");
+ res.status(200).send(d);
+}
